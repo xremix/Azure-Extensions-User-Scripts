85d5eb4b531048c53e9e0ad78f9f482d383dcbcb
Toni Hoffmann <toni.hoffmann@arthrex.de>
2024-09-16 09:33:10 +0200

    clean up

--- main.js
+++ main.js
@@ -10,26 +10,25 @@
 // ==/UserScript==
 
 function extendAutoExpandNavigationGroupings() {
-
   const observer = new MutationObserver(() => {
-  // Get all groupings
-  let groupings = Array.from(document.querySelectorAll('button.azc-listView-collapsible-groupheader'));
+    // Get all groupings
+    let groupings = Array.from(document.querySelectorAll('button.azc-listView-collapsible-groupheader'));
 
-  // Filter groupings to only those that contain the relevant SVG child
-  let relevantGroupings = groupings.filter((grouping) => {
-    let svg = grouping.querySelector('svg');
-    return svg && svg.firstChild && svg.firstChild.href.baseVal.includes('#FxSymbol0-034');
+    // Filter groupings to only those that contain the relevant SVG child
+    let relevantGroupings = groupings.filter((grouping) => {
+      let svg = grouping.querySelector('svg');
+      return svg && svg.firstChild && svg.firstChild.href.baseVal.includes('#FxSymbol0-034');
+    });
+
+    // Print the count of relevant groupings
+    console.log(`Found ${relevantGroupings.length} relevant groupings`);
+
+    // Click through each relevant grouping
+    relevantGroupings.forEach((grouping) => {
+      grouping.click();
+    });
   });
-
-  // Print the count of relevant groupings
-  console.log(`Found ${relevantGroupings.length} relevant groupings`);
-
-  // Click through each relevant grouping
-  relevantGroupings.forEach((grouping) => {
-    grouping.click();
-  });
-});
-observer.observe(document.body, { childList: true });
+  observer.observe(document.body, { childList: true });
 }
 
 (function () {
